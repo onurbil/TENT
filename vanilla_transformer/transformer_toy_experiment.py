4888ebd889bf325c3122db01ded700dcbab2229e
Pawel Maka <pawel.maka@gmail.com>
2020-10-30 14:41:14 +0100

    Added vanilla transformer. Added toy experiment using vanilla transformer.

---
+++ vanilla_transformer/transformer_toy_experiment.py
@@ -0,0 +1,92 @@
+import time
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as kr
+
+import vanilla_transformer as vt
+
+train_x = np.array(
+    [[
+        [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        ],
+        [
+            [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        ],
+        [
+            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+        ],
+        [
+            [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        ],
+        [
+            [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+        ],
+        [
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+        ],
+        [
+            [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+        ],
+        [
+            [0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+        ],
+        [
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        ],
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        ],
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ],
+    ]]
+    , dtype=np.float32)
+
+train_y = np.array(
+    [[[
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+    ]]]
+    , dtype=np.float32)
+
+EPOCHS = 2000
+num_layers = 6
+d_model = train_x.shape[-1]
+dff = 64
+num_heads = 10
+input_size = train_x.shape[-2]
+output_size = train_y.shape[-1]
+dropout_rate = 0.1
+
+transformer = vt.Transformer(num_layers, d_model, num_heads, dff, input_size, output_size, rate=dropout_rate)
+learning_rate = vt.CustomSchedule(d_model)
+optimizer = kr.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+# train_loss = kr.metrics.Mean(name='train_loss')
+
+transformer.fit(train_x, train_y, EPOCHS, optimizer, loss=kr.losses.MeanSquaredError())
+
+pred = transformer(train_x[0], False)[0]
+print(np.round(pred.numpy()))
