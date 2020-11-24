6dcb56e805935dbbcf95d77826ac0d677b39763c
ob <onurbil@hotmail.com>
2020-11-24 21:07:39 +0100

    attention plotter

--- visualization_tools/visualization.py
+++ visualization_tools/visualization.py
@@ -1,11 +1,17 @@
+import numpy as np
 import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 import datetime
 from tensorflow.keras.callbacks import TensorBoard
+import seaborn as sns
+from matplotlib.patches import Rectangle
+from debugging_tools import *
 
 def visualize_pos_encoding(array):
     """
-    Plot heatmap of positional encoding. 
+    Plot heatmap of positional encoding for the array.
     """
     plt.pcolormesh(array, cmap='viridis')
     plt.xlabel('Embedding Dimensions')
@@ -14,6 +20,33 @@ def visualize_pos_encoding(array):
     plt.ylim((array.shape[0],0))
     plt.colorbar()
     plt.show()
+
+
+def attention_plotter(attention_weights, y_labels, save=False):
+    """
+    attention_weights: Numpy array with shape (batch,number of attention heads)
+    y_labels: Labels of y-axis (here: daily hours) in alist with batch length.
+    """
+    assert(attention_weights.shape[0]==len(y_labels)
+           ), 'Attention weight has size {} and labels has size {}!'.format(
+           attention_weights.shape[0], len(y_labels))
+    # Get for each column the cell with maximal attention:
+    max_attention = np.argmax(attention_weights, axis=0)
+        
+    ax = sns.heatmap(attention_weights, annot=True, cmap='Blues')
+    ax.set(xlabel='Attention Heads', ylabel='Dates',
+           title='Visualization of Attention Weights')
+    bottom, top = ax.get_ylim()
+    ax.set_ylim(bottom + 0.5, top - 0.5)
+    ax.set_yticklabels(y_labels, rotation=0)
+    # Draw rectangle around the cell with maximum attention:
+    for col, variable in enumerate(max_attention):
+        ax.add_patch(Rectangle((col,variable),1,1,
+                     fill=False, edgecolor='red', lw=3))
+    plt.show()
+    if save:
+        fig = ax.get_figure()
+        fig.savefig("Attention Visualization.png")
 
 
 # To call tensorboard:
