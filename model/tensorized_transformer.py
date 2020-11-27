7a3880c1bcd220a332f0c5263c866a64408fee93
Pawel-M <pawel.maka@gmail.com>
2020-11-27 18:34:22 +0100

    Added weights of EncoderLayer to be seen and trained by tensorflow.

--- model/tensorized_transformer.py
+++ model/tensorized_transformer.py
@@ -9,18 +9,77 @@ import attention.self_attention
 import common.paths
 
 
+# class SelfAttentionLayer(kr.layers.Layer):
+#     def __init__(self,
+#                  input_length,
+#                  d_model,
+#                  head_num,
+#                  input_shape,
+#                  #mask,
+#                  **kwargs):
+#         super(SelfAttentionLayer, self).__init__(**kwargs)
+#
+#         self.input_length = input_length
+#         self.d_model = d_model
+#         self.head_num = head_num
+#
+#         self.wq = None
+#         self.wk = None
+#         self.wv = None
+#         self.wo = None
+#
+#     def build(self, input_shape):
+#         # self.z_all = tf.zeros([input_shape[-2], input_shape[-1], 0])
+#         self.wq = tf.random.normal([input_shape[-1], d_model], mean=0.0, stddev=1.0)
+#         self.wk = tf.random.normal([input_shape[-1], d_model], mean=0.0, stddev=1.0)
+#         self.wv = tf.random.normal([input_shape[-1], d_model], mean=0.0, stddev=1.0)
+#         self.wo = tf.random.normal([self.input_length, self.d_model, input_shape[-1]], mean=0.0, stddev=1.0)
+#
+#     def call(self, inputs):
+#         q = tf.matmul(inputs, self.wq)
+#         k = tf.matmul(inputs, self.wk)
+#         v = tf.matmul(inputs, self.wv)
+#
+#         qe = tf.broadcast_to(q, (q.shape[-3], q.shape[-3], q.shape[-2], q.shape[-1]))
+#         kt = tf.transpose(k, perm=(0, 1, 3, 2))
+#         kt = tf.broadcast_to(kt, (kt.shape[-3], kt.shape[-3], kt.shape[-2], kt.shape[-1]))
+#         z = tf.matmul(qe, kt)
+#
+#         dk = tf.cast(tf.shape(k)[-1], tf.float32)
+#         z = z / tf.math.sqrt(dk)
+#         # if mask is not None:
+#         #     z += (mask * -1e9)
+#
+#         z = tf.reduce_sum(z, axis=[-1, -2])
+#         se = tf.nn.softmax(z, axis=-1)
+#         se = tf.expand_dims(se, -1)
+#         se = tf.expand_dims(se, -1)
+#
+#         ve = tf.broadcast_to(v, [v.shape[-3], v.shape[-3], v.shape[-2], v.shape[-1]])
+#         se = tf.broadcast_to(se, (v.shape[-3], v.shape[-3], v.shape[-2], v.shape[-1]))
+#         # z = tf.matmul(se, v)
+#         z = tf.multiply(se, ve)
+#         z = tf.reduce_sum(z, axis=1)
+#
+#         z = tf.matmul(z, self.wo)
+#
+#         return z
+
+
 class EncoderLayer(kr.layers.Layer):
     def __init__(self,
                  input_length,
                  d_model,
                  head_num,
                  dense_units,
+                 # initializer='he normal',
                  **kwargs):
         super(EncoderLayer, self).__init__(**kwargs)
 
         self.input_length = input_length
         self.d_model = d_model
         self.head_num = head_num
+        # self.initializer = keras.initializers.get(initializer)
 
         self.wq = None
         self.wk = None
@@ -34,10 +93,30 @@ class EncoderLayer(kr.layers.Layer):
 
     def build(self, input_shape):
         self.z_all = tf.zeros([input_shape[-2], input_shape[-1], 0])
-        self.wq = tf.random.normal([input_shape[-1], d_model], mean=0.0, stddev=1.0)
-        self.wk = tf.random.normal([input_shape[-1], d_model], mean=0.0, stddev=1.0)
-        self.wv = tf.random.normal([input_shape[-1], d_model], mean=0.0, stddev=1.0)
-        self.wo = tf.random.normal([self.input_length, self.d_model, input_shape[-1]], mean=0.0, stddev=1.0)
+        self.wq = self.add_weight(
+            shape=(input_shape[-1], d_model),
+            initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=1., seed=None),
+            name="wq",
+            trainable=True,
+        )
+        self.wk = self.add_weight(
+            shape=(input_shape[-1], d_model),
+            initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=1., seed=None),
+            name="wk",
+            trainable=True,
+        )
+        self.wv = self.add_weight(
+            shape=(input_shape[-1], d_model),
+            initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=1., seed=None),
+            name="wv",
+            trainable=True,
+        )
+        self.wo = self.add_weight(
+            shape=(self.input_length, self.d_model, input_shape[-1]),
+            initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=1., seed=None),
+            name="wo",
+            trainable=True,
+        )
         self.dense_out = tf.keras.layers.Dense(tf.reduce_prod(input_shape[-3:]), activation='relu')
         self.reshape = tf.keras.layers.Reshape(input_shape[-3:])
 
@@ -101,32 +180,31 @@ class EncoderLayer(kr.layers.Layer):
 #     pass
 
 
-# # Load dataset:
-# filename = 'dataset_tensor.npy'
-# file_path = os.path.join(common.paths.PROCESSED_DATASET_DIR, filename)
-# dataset = np.load(file_path, allow_pickle=True)
-#
-# # Get x_train, y_train, x_test, y_test:
-# input_length = 24
-# train, test = dataset_tools.split.split_train_test(dataset)
-# x_train, y_train = dataset_tools.split.get_xy(train, input_length=input_length)
-# x_test, y_test = dataset_tools.split.get_xy(test, input_length=input_length)
-#
-# x_train = x_train.astype('float32')
-# x_train = tf.reshape(x_train, (x_train.shape[0], x_train.shape[1], dataset.shape[1], dataset.shape[2]))
-# y_train = tf.reshape(y_train, (y_train.shape[0], dataset.shape[1], dataset.shape[2]))
-# x_test = tf.reshape(x_test, (x_test.shape[0], x_test.shape[1], dataset.shape[1], dataset.shape[2]))
-# y_test = tf.reshape(y_test, (y_test.shape[0], dataset.shape[1], dataset.shape[2]))
+# Load dataset:
+filename = 'dataset_tensor.npy'
+file_path = os.path.join(common.paths.PROCESSED_DATASET_DIR, filename)
+dataset = np.load(file_path, allow_pickle=True)
+
+# Get x_train, y_train, x_test, y_test:
+input_length = 24
+train, test = dataset_tools.split.split_train_test(dataset)
+x_train, y_train = dataset_tools.split.get_xy(train, input_length=input_length)
+x_test, y_test = dataset_tools.split.get_xy(test, input_length=input_length)
+
+x_train = x_train.astype('float32')
+x_train = tf.reshape(x_train, (x_train.shape[0], x_train.shape[1], dataset.shape[1], dataset.shape[2]))
+y_train = tf.reshape(y_train, (y_train.shape[0], dataset.shape[1], dataset.shape[2]))
+x_test = tf.reshape(x_test, (x_test.shape[0], x_test.shape[1], dataset.shape[1], dataset.shape[2]))
+y_test = tf.reshape(y_test, (y_test.shape[0], dataset.shape[1], dataset.shape[2]))
 
 
 input_length = 24
-d_model = 8
+d_model = 10
 head_num = 2
 dense_units = 64
-input_shape = (24, 33, 6)
-output_shape = (33, 6)
+input_shape = (24, 36, 6)
+output_shape = (36, 6)
 
-x_train = tf.ones((1, 24, 33, 6))
 
 model = kr.Sequential([
     kr.Input(input_shape),
@@ -134,10 +212,19 @@ model = kr.Sequential([
     EncoderLayer(input_length, d_model, head_num, dense_units),
     EncoderLayer(input_length, d_model, head_num, dense_units),
     kr.layers.Flatten(),
-    kr.layers.Dense(tf.reduce_prod(output_shape), activation='relu'),
+    kr.layers.Dense(tf.reduce_prod(output_shape), activation='linear'),
     kr.layers.Reshape(output_shape),
 ])
 
 model.summary()
-pred = model.predict(x_train)
+model.compile(optimizer='adam', loss='mse')
+
+num_examples = 1000
+x_train = x_train[:num_examples]
+y_train = y_train[:num_examples]
+print(x_train.shape, y_train.shape)
+
+model.fit(x_train, y_train, epochs=100, batch_size=1)
+
+pred = model.predict(x_train[0][np.newaxis, ...])
 print(pred.shape)
