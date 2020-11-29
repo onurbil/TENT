7c36638e49a02cf80c5dd4c093d1a158bc8d1781
Pawel-M <pawel.maka@gmail.com>
2020-11-29 12:31:02 +0100

    Added positional encoding based on broadcasting.

--- model/tensorized_transformer.py
+++ model/tensorized_transformer.py
@@ -10,7 +10,6 @@ import common.paths
 from visualization_tools.visualization import visualize_pos_encoding
 
 
-
 # class SelfAttentionLayer(kr.layers.Layer):
 #     def __init__(self,
 #                  input_length,
@@ -71,50 +70,38 @@ def get_angles(pos, i, d_model):
     angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
     return pos * angle_rates
 
+
 class PositionalEncoding(kr.layers.Layer):
     def __init__(self,
-                 input_length,
-                 d_model,
+                 broadcast,
                  **kwargs):
         super(PositionalEncoding, self).__init__(**kwargs)
-        self.input_length = input_length
-        self.d_model = d_model
-        self.reshape: tf.keras.layers.Reshape = None
+        self.broadcast = broadcast
 
     def build(self, input_shape):
         self.position = input_shape[-3]
-        # self.d_model = input_shape[-1]
-        # self.reshape = tf.keras.layers.Reshape(input_shape[-3:])
         self.angle_dim = input_shape[-2]
         if not self.broadcast:
             self.angle_dim *= input_shape[-1]
-        # self.output_dim = tf.zeros([input_shape[-2], input_shape[-1], 0])
 
-        # super(PositionalEncoding, self).build(input_shape)
+        self.model_shape = input_shape
 
     def call(self, input_data):
-
-        # position = self.model_shape[0]
-        
-
         angle_rads = get_angles(np.arange(self.position)[:, np.newaxis],
                                 np.arange(self.angle_dim)[np.newaxis, :],
                                 self.angle_dim)
-        # apply sin to even indices in the array; 2i
         angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
-        # apply cos to odd indices in the array; 2i+1
         angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
 
         if self.broadcast:
-            angle_rads = np.broadcast_to(np.expand_dims(angle_rads, -1), angle_rads.shape + (self.model_shape[1],))
+            angle_rads = np.broadcast_to(np.expand_dims(angle_rads, -1), input_data.shape[1:])
         else:
             new_shape = angle_rads.shape[:-1] + self.model_shape
             angle_rads = np.reshape(angle_rads, new_shape)
 
         pos_encoding = angle_rads[np.newaxis, ...]
+        return tf.math.add(input_data, pos_encoding)
 
-        return tf.cast(pos_encoding, dtype=tf.float32)
-        
 
 class EncoderLayer(kr.layers.Layer):
     def __init__(self,
@@ -222,6 +209,7 @@ class EncoderLayer(kr.layers.Layer):
 
         return z
 
+
 # def get_config(self):
 #     # Implement get_config to enable serialization. This is optional.
 #     # base_config = super(Antirectifier, self).get_config()
@@ -252,48 +240,30 @@ input_length = 24
 d_model = 10
 head_num = 2
 dense_units = 64
-input_shape = (24, 36, 6)
+input_shape = (input_length, 36, 6)
 output_shape = (36, 6)
 initializer = 'RandomNormal'
 
 model = kr.Sequential([
     kr.Input(input_shape),
-    # positional encoding
-    PositionalEncoding(input_length, d_model),
-    # EncoderLayer(input_length, d_model, head_num, dense_units, initializer),
-    # EncoderLayer(input_length, d_model, head_num, dense_units, initializer),
-    # kr.layers.Flatten(),
-    # kr.layers.Dense(tf.reduce_prod(output_shape), activation='linear'),
-    # kr.layers.Reshape(output_shape),
+    PositionalEncoding(broadcast=True),
+    EncoderLayer(input_length, d_model, head_num, dense_units, initializer),
+    EncoderLayer(input_length, d_model, head_num, dense_units, initializer),
+    kr.layers.Flatten(),
+    kr.layers.Dense(tf.reduce_prod(output_shape), activation='linear'),
+    kr.layers.Reshape(output_shape),
 ])
 
 model.summary()
-model.compile(optimizer='adam', loss='mse')
+model.compile(optimizer='adam', loss='mae')
 
 num_examples = 1000
 x_train = x_train[:num_examples]
 y_train = y_train[:num_examples]
 print(x_train.shape, y_train.shape)
 
-# model.fit(x_train, y_train, epochs=100, batch_size=1)
+model.fit(x_train, y_train, epochs=100, batch_size=1)
 
-aa = x_train[0][np.newaxis, ...]
-aa = np.zeros((aa.shape))
-test = np.zeros((1,24,36,6))
-pred = model.predict(test)
+pred = model.predict(x_test[0])
 print(pred.shape)
 
-
-# bb = positional_encoding(pred.shape[0],test.shape, broadcast=True)
-
-# bb = bb.numpy()
-pred = pred[0]
-print(pred.shape)
-bb = pred[0].reshape((pred.shape[0],-1))
-print(bb.shape)
-visualize_pos_encoding(bb)
-
-# 
-# bb = pred
-# bb = bb[0].reshape((bb.shape[3],-1))
-# visualize_pos_encoding(bb)
