4228387cf69a124c956d9a9831f904324c99c0e0
Thomas Vergutz <tvergut@gmail.com>
2021-01-03 17:36:35 +0100

    Changing custom learning rate

--- model/tt_mode_weights_TPU.py
+++ model/tt_mode_weights_TPU.py
@@ -243,19 +243,20 @@ def custom_loss_function(lambada):
 
 
 class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, d_model, warmup_steps=60):
+    def __init__(self, d_model, warmup_steps=60, factor=-0.5):
         super(CustomSchedule, self).__init__()
 
         self.d_model = d_model
         self.d_model = tf.cast(self.d_model, tf.float32)
 
         self.warmup_steps = warmup_steps
+        self.factor = factor
 
     def __call__(self, step):
         arg1 = tf.math.rsqrt(step)
         arg2 = step * (self.warmup_steps ** -1.5)
 
-        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+        return (self.d_model ** self.factor) * tf.math.minimum(arg1, arg2)
 
 
 if __name__ == '__main__':
