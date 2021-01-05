81d94788b14b3b66c076e8c177df75cd1b54c48d
Onur Bilgin <onurbil@hotmail.com>
2021-01-05 23:06:36 +0100

    word embedding example

--- dataset_tools/example_word_embedding.py
+++ dataset_tools/example_word_embedding.py
@@ -1,6 +1,7 @@
 from numpy import array
 from numpy import asarray
 from numpy import zeros
+import keras
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
@@ -8,7 +9,15 @@ from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers import Embedding
 
-# Weather description:
+
+"""
+Example:
+
+Download glove.6B.zip file from:
+https://nlp.stanford.edu/projects/glove/
+"""
+
+
 docs = ['overcast clouds', 'sky is clear', 'broken clouds', 'fog', 'mist',
  'scattered clouds', 'few clouds', 'light rain', 'light intensity drizzle',
  'moderate rain', 'light intensity shower rain', 'haze', 'heavy shower snow',
@@ -20,28 +29,9 @@ docs = ['overcast clouds', 'sky is clear', 'broken clouds', 'fog', 'mist',
  'light shower snow', 'shower rain', 'shower drizzle', 'sand',
  'thunderstorm with heavy rain', 'heavy intensity shower rain', 'drizzle']
 
-
-"""
-Example:
-
-Download glove.6B.zip file from:
-https://nlp.stanford.edu/projects/glove/
-"""
-
-# define documents
-docs = ['Well done!',
-		'Good work',
-		'Great effort',
-		'nice work',
-		'Excellent!',
-		'Weak',
-		'Poor effort!',
-		'not good',
-		'poor work',
-		'Could have done better.']
+class_count = len(docs)
+labels = np.arange(class_count)
         
-# define class labels
-labels = array([1,1,1,1,1,0,0,0,0,0])
 # prepare tokenizer
 t = Tokenizer()
 t.fit_on_texts(docs)
@@ -66,21 +56,25 @@ print('Loaded %s word vectors.' % len(embeddings_index))
 # create a weight matrix for words in training docs
 embedding_matrix = zeros((vocab_size, 100))
 for word, i in t.word_index.items():
-	embedding_vector = embeddings_index.get(word)
-	if embedding_vector is not None:
-		embedding_matrix[i] = embedding_vector
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
 # define model
 model = Sequential()
 e = Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=4, trainable=False)
 model.add(e)
 model.add(Flatten())
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(class_count, activation='sigmoid'))
 # compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
 # summarize the model
 print(model.summary())
 # fit the model
-model.fit(padded_docs, labels, epochs=50, verbose=0)
+# debug(padded_docs.reshape((-1,1)))
+labels = keras.utils.to_categorical(labels, len(docs))
+model.fit(padded_docs, labels, epochs=100, verbose=1)
 # evaluate the model
 loss, accuracy = model.evaluate(padded_docs, labels, verbose=0)
 print('Accuracy: %f' % (accuracy*100))
