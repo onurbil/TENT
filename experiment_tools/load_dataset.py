421efb8af94c2ac4d9c963905b3bace13873443e
Pawel Maka <pawel.maka@gmail.com>
2021-02-12 17:26:44 +0100

    Moved functions from experiment notebooks to python files.

---
+++ experiment_tools/load_dataset.py
@@ -0,0 +1,115 @@
+import os.path
+
+import numpy as np
+import sklearn.model_selection
+import scipy.io
+
+import common.paths
+import dataset_tools.split
+
+
+def get_usa_dataset(data_path, lag, step, y_feature, y_city, start_city=0, end_city=30, remove_last_from_test=0,
+                    valid_split=None, split_random=None):
+    filename = os.path.join(data_path, 'dataset_tensor.npy')
+    dataset = np.load(filename, allow_pickle=True)
+
+    print(dataset.shape)
+    city_feature_shape = (dataset.shape[1], dataset.shape[2])
+
+    train, test = dataset_tools.split.split_train_test(dataset)
+    x_train, y_train = dataset_tools.split.get_xy(train, input_length=lag, pred_time=step)
+    x_test, y_test = dataset_tools.split.get_xy(test, input_length=lag, pred_time=step)
+
+    print(x_train.shape, y_train.shape)
+    if valid_split is not None:
+        x_train, x_valid, y_train, y_valid = sklearn.model_selection.train_test_split(x_train, y_train,
+                                                                                      test_size=valid_split,
+                                                                                      random_state=split_random)
+        x_valid, y_valid = prepare_usa_xy(x_valid, y_valid, city_feature_shape,
+                                          start_city, end_city, y_city, y_feature)
+
+    x_train, y_train = prepare_usa_xy(x_train, y_train, city_feature_shape, start_city, end_city, y_city, y_feature)
+    x_test, y_test = prepare_usa_xy(x_test, y_test, city_feature_shape, start_city, end_city, y_city, y_feature)
+
+    x_test = x_test[:-remove_last_from_test, ...]
+    y_test = y_test[:-remove_last_from_test, ...]
+
+    print(f'FULL_x_train.shape: {x_train.shape}')
+
+    if valid_split is None:
+        return x_train, y_train, x_test, y_test
+    else:
+        return x_train, y_train, x_valid, y_valid, x_test, y_test
+
+
+def prepare_usa_xy(x, y, city_feature_shape, start_city, end_city, y_city, y_feature):
+    x = np.reshape(x, (x.shape[0], x.shape[1], city_feature_shape[0], city_feature_shape[1]))
+    y = np.reshape(y, (y.shape[0], city_feature_shape[0], city_feature_shape[1]))
+    x = x[:, :, start_city:end_city, :]
+    y = y[:, start_city:end_city, :]
+    y = np.expand_dims(y[..., y_city, y_feature], axis=-1)
+    return x, y
+
+
+def get_denmark_dataset(data_path, step, feature, y_city=None, valid_split=None, split_random=None):
+    filename = os.path.join(data_path, 'Denmark')
+    filename = os.path.join(filename, f'{feature}')
+    filename = os.path.join(filename, f'step{step}.mat')
+    mat = scipy.io.loadmat(filename)
+    Xtr = mat['Xtr'].swapaxes(1, 2)
+    Ytr = mat['Ytr']
+    Xtest = mat['Xtest'].swapaxes(1, 2)
+    Ytest = mat['Ytest']
+    if y_city is not None:
+        Ytr = Ytr[:, y_city:y_city + 1]
+        Ytest = Ytest[:, y_city:y_city + 1]
+
+    if valid_split is None:
+        return Xtr, Ytr, Xtest, Ytest
+    else:
+        Xtr, Xvalid, Ytr, Yvalid = sklearn.model_selection.train_test_split(Xtr, Ytr, test_size=valid_split,
+                                                                            random_state=split_random)
+        return Xtr, Ytr, Xvalid, Yvalid, Xtest, Ytest
+
+
+def to_flatten_dataset(Xtr, Xtest, Xvalid=None):
+    t = Xtr.shape[1]
+    f = Xtr.shape[2] * Xtr.shape[3]
+
+    Xtr = Xtr.reshape((Xtr.shape[0], t, f))
+    Xtest = Xtest.reshape((Xtest.shape[0], t, f))
+    if Xvalid is None:
+        return Xtr, Xtest
+    else:
+        print(Xvalid.shape)
+        Xvalid = Xvalid.reshape((Xvalid.shape[0], t, f))
+        return Xtr, Xtest, Xvalid
+
+
+def reshape_to_batches(Xs, Ys, batch_size):
+    num_batches = Xs.shape[0] // batch_size
+    batched_len = num_batches * batch_size
+    print(Ys.shape)
+
+    Xs = Xs[:batched_len, ...].reshape((num_batches, batch_size) + Xs.shape[1:])
+    Ys = Ys[:batched_len, ...].reshape((num_batches, batch_size) + Ys.shape[1:])
+
+    return Xs, Ys
+
+
+if __name__ == '__main__':
+    Xtr, Ytr, Xvalid, Yvalid, Xtest, Ytest = get_usa_dataset(data_path=common.paths.PROCESSED_DATASET_DIR,
+                                                             lag=16, step=4, y_feature=4, y_city=0, start_city=0,
+                                                             end_city=30, remove_last_from_test=800, valid_split=1024,
+                                                             split_random=None)
+
+    print(Xtr.shape, Ytr.shape, Xtest.shape, Ytest.shape, Xvalid.shape, Yvalid.shape)
+    Xtr, Xtest, Xvalid = to_flatten_dataset(Xtr, Xtest, Xvalid)
+    Xtr, Ytr = reshape_to_batches(Xtr, Ytr, 128)
+    print(Xtr.shape, Ytr.shape, Xtest.shape, Ytest.shape, Xvalid.shape, Yvalid.shape)
+
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=(40, 8))
+    plt.plot(range(Ytest.size), Ytest.flatten())
+    plt.show()
---
+++ experiment_tools/results.py
@@ -0,0 +1,67 @@
+import os
+import datetime
+
+import tensorflow.keras as kr
+import numpy as np
+import matplotlib.pyplot as plt
+
+import dataset_tools.denormalization as denorm
+
+
+def plot_predictions(Ys, pred, folder, name, ending):
+    fileName = folder + name
+    pred = pred.flatten()
+    Ys = Ys.flatten()
+    mae = kr.metrics.mae(Ys, pred)
+    mse = kr.metrics.mse(Ys, pred)
+    f = open(fileName + ".txt", "a")
+    print(f'Figure mae{ending}: {np.mean(mae)}', file=f)
+    print(f'Figure mse{ending}: {np.mean(mse)}', file=f)
+    print('\n\n', file=f)
+    f.close()
+    print(f'Figure mae: {np.mean(mae)}')
+    print(f'Figure mse: {np.mean(mse)}')
+
+    plot_width = 20 if Ys.size < 1000 else 100
+    plt.figure(figsize=(plot_width, 8))
+    plt.plot(range(pred.size), pred, label='pred')
+    plt.plot(range(len(Ys)), Ys, label='true')
+    plt.legend()
+    plt.savefig(fileName + ending)
+    plt.show()
+
+
+def save_results(parameters, model, folder, name):
+    fileName = os.path.join(folder, name)
+
+    os.makedirs(os.path.dirname(folder), exist_ok=True)
+    with open(fileName + ".txt", "a") as fh:
+        model.summary(print_fn=lambda x: fh.write(x + '\n'))
+    f = open(fileName + ".txt", "a")
+    print("\n\n######################## Model description ################################", file=f)
+    for name, results in parameters:
+        print(str(name) + " = " + str(results), file=f)
+    print("######################### Results ##########################################", file=f)
+    f.close()
+
+
+def save_results_with_datetime(model, base_name, path, parameters):
+    date = datetime.datetime.now().strftime("%Y_%m_%d")
+    time = datetime.datetime.now().strftime("%H_%M_%S")
+    name = f'{base_name}_{time}'
+    folder = os.path.join(path, 'Tests/')
+    folder = os.path.join(folder,  f'{date}')
+
+    save_results(parameters, model, folder, name)
+
+
+def plot_valid_test_predictions(model, Xvalid, Yvalid, Xtest, Ytest, y_feature, folder, base_name):
+    pred = model.predict(Xvalid)
+    plot_predictions(Yvalid, pred, folder, base_name, '_valid.png')
+
+    pred = model.predict(Xtest)
+    plot_predictions(Ytest, pred, folder, base_name, '_test.png')
+
+    pred_denorm = denorm.denormalize_feature(pred, y_feature)
+    Ytest_denorm = denorm.denormalize_feature(Ytest, y_feature)
+    plot_predictions(Ytest_denorm, pred_denorm, folder, base_name, '_1.png')
