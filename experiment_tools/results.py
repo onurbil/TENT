00c8c5a69b1b99ca5a55cfe2e73643938516538d
Thomas Vergutz <tvergut@gmail.com>
2021-02-15 16:31:14 +0100

    Fixing save_results

--- experiment_tools/results.py
+++ experiment_tools/results.py
@@ -32,12 +32,12 @@ def save_results(parameters, model, folder, name):
     fileName = os.path.join(folder, name)
     print(f'saving to folder: {folder}')
     os.makedirs(os.path.dirname(folder), exist_ok=True)
-    if name.startswith('MultiConv') or name.startswith('CNN3D'):
+    if name.startswith('MultiConv'):
         f = open(fileName + ".txt", "a")
         print('Model Summury not implemented', file = f)
     else:
         with open(fileName + ".txt", "a") as fh:
-            model.summary(summary_str=lambda x: fh.write(x + '\n'))
+            model.summary(print_fn=lambda x: fh.write(x + '\n'))
         f = open(fileName + ".txt", "a")
     print("\n\n######################## Model description ################################", file=f)
     for name, results in parameters:
